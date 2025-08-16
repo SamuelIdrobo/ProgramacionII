8ac8d01bd001f0a337c7565f5fad2854574b4fa2
SamuelIdrobo <sidrobovictoria@gmail.com>
2025-08-15 23:46:35 -0500

    Calculadora
    1. Crear un programa que utilice estructuras de control para resolver un problema sencillo, como una calculadora básica que realice operaciones de suma, resta, multiplicación y división

---
+++ VocalesYConsonantes.java
@@ -0,0 +1,22 @@
+// 2. Escribir un programa que cuente el número de vocales y consonantes en una palabra.
+// La palabra no contendrá símbolos, caracteres especiales, acentos, ni números y siempre estará en minúsculas.
+
+import java.util.Scanner; 
+
+public class VocalesYConsonantes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        
+
+
+
+
+
+
+        scanner.close();
+    }
+
+}
+
